7276b2c0b89b5d20d6ffb27180bdcd82cb334ad4
Leonardo Delgado <joad1654321@gmail.com>
2026-06-19 20:15:55 -0300

    Clase Persona
    ↑↑↑

---
+++ ENTIDADES/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public abstract class Persona
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public char Sexo { get; set; }   
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public Localidad Localidad { get; set; } 
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public bool Activo { get; set; }
+
+        public string NombreCompleto => $"{Apellido}, {Nombre}";
+    }
+}
