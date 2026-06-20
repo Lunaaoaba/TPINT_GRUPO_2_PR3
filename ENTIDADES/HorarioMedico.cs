fcf2f9805e0907426d2e2173a425cd36d76b7315
Leonardo Delgado <joad1654321@gmail.com>
2026-06-19 21:44:45 -0300

    Clase HorarioMedico
    ↑↑↑

---
+++ ENTIDADES/HorarioMedico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class HorarioMedico
+    {
+        public int HorarioId { get; set; }
+        public int MedicoId { get; set; }
+        public int DiaSemana { get; set; }     // 1=Lunes ... 7=Domingo
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public bool Activo { get; set; }
+
+        public string NombreDia
+        {
+            get
+            {
+                string[] dias = { "", "Lunes", "Martes", "Miercoles",
+                                   "Jueves", "Viernes", "Sabado", "Domingo" };
+                return (DiaSemana >= 1 && DiaSemana <= 7) ? dias[DiaSemana] : "";
+            }
+        }
+    }
+}
