e2b84c93859488f02a8bdfe5d6cfc28f8347b6e0
Lunaaoaba <lunabaricalla1@gmail.com>
2026-06-27 20:33:01 -0300

    Update consultas.sql

--- BDD_Clinica/consultas.sql
+++ BDD_Clinica/consultas.sql
@@ -3,11 +3,7 @@ GO
 
 -- CONSULTAS PACIENTE --
 
-SELECT id_pac, dni_pac, nombre_pac, apellido_pac, sexo_pac, nacionalidad_pac, fecha_nacimiento_pac, direccion_pac, id_loc, email_pac, telefono_pac, activo_pac FROM PACIENTE;
-GO
-
---eliminado baja logica
-CREATE PROCEDURE spEliminarPaciente
+CREATE PROCEDURE [spEliminarPaciente]
     @id_pac INT
 AS
 BEGIN
@@ -17,8 +13,8 @@ BEGIN
 END
 GO
 
---agregado
-CREATE PROCEDURE spAgregarPaciente
+
+CREATE PROCEDURE [spAgregarPaciente]
     @dni_pac VARCHAR(15),
     @nombre_pac VARCHAR(50),
     @apellido_pac VARCHAR(50),
@@ -40,8 +36,8 @@ BEGIN
 END
 GO
 
---modificacion
-CREATE PROCEDURE spModificarPaciente
+
+CREATE PROCEDURE [spModificarPaciente]
     @id_pac INT,
     @dni_pac VARCHAR(15),
     @nombre_pac VARCHAR(50),
@@ -67,5 +63,99 @@ BEGIN
         email_pac = @email_pac,
         telefono_pac = @telefono_pac
     WHERE id_pac = @id_pac
+END
+GO
+
+
+-- CONSULTAS MEDICO --
+
+CREATE PROCEDURE [spAgregarMedico]
+    @DniMedico VARCHAR(15),
+    @NombreMedico VARCHAR(50),
+    @ApellidoMedico VARCHAR(50),
+    @SexoMedico CHAR(1),
+    @NacionalidadMedico VARCHAR(50),
+    @FechaNacimientoMedico DATE,
+    @DireccionMedico VARCHAR(100),
+    @IdLocalidad INT,
+    @EmailMedico VARCHAR(100),
+    @TelefonoMedico VARCHAR(30),
+    @LegajoMedico VARCHAR(20),
+    @IdEspecialidad INT,
+    @IdUsuario INT,
+    @ActivoMedico BIT
+AS
+BEGIN
+    INSERT INTO MEDICO
+    (
+        id_usu,
+        legajo_med,
+        dni_med,
+        nombre_med,
+        apellido_med,
+        sexo_med,
+        nacionalidad_med,
+        fecha_nacimiento_med,
+        direccion_med,
+        id_loc,
+        email_med,
+        telefono_med,
+        id_esp,
+        activo_med
+    )
+    VALUES
+    (
+        @IdUsuario,
+        @LegajoMedico,
+        @DniMedico,
+        @NombreMedico,
+        @ApellidoMedico,
+        @SexoMedico,
+        @NacionalidadMedico,
+        @FechaNacimientoMedico,
+        @DireccionMedico,
+        @IdLocalidad,
+        @EmailMedico,
+        @TelefonoMedico,
+        @IdEspecialidad,
+        @ActivoMedico
+    )
+END
+GO
+
+
+CREATE PROCEDURE [spEliminarMedico]
+    @id_med INT
+AS
+BEGIN
+    UPDATE MEDICO
+    SET activo_med = 0
+    WHERE id_med = @id_med
+END
+GO
+
+CREATE PROCEDURE spAgregarUsuario
+    @Username VARCHAR(30),
+    @Password VARCHAR(100),
+    @Tipo VARCHAR(10),
+    @Activo BIT
+AS
+BEGIN
+    INSERT INTO USUARIO
+    (
+        username_usu,
+        password_usu,
+        tipo_usu,
+        activo_usu
+    )
+    VALUES
+    (
+        @Username,
+        @Password,
+        @Tipo,
+        @Activo
+    );
+
+    SELECT SCOPE_IDENTITY() AS id_usu;
 END
 GO
